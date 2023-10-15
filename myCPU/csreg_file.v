a6bb6b10520cb1c725e9bb70cb222def91ca95bf
yzcccccccccc <yangzechen21@mails.ucas.ac.cn>
2023-10-15 21:31:28 +0800

    init exp12

---
+++ myCPU/csreg_file.v
@@ -0,0 +1,16 @@
+/*****************************************************
+            Control and State Reg File
+    - version1.0
+    - 2023.10.15, created by yzcc
+******************************************************/
+
+module csreg_file(
+    input   wire        clk,
+    input   wire        reset,
+    input   wire        csr_re,         // enable
+    input   wire [13:0] csr_num,        // addr
+    output  wire [31:0] csr_rvalue,     // return value
+    input   wire 
+);
+
+endmodule
